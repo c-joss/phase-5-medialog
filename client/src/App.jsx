ee584403e566cec536fc8c1a732ee83b688cb823
Courtney Macgregor <cjoss90@hotmail.com>
2025-11-24 16:14:32 +1000

    feat(frontend): add route and scaffold for CreateItemPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import LoginPage from './pages/LoginPage';
 import ItemsPage from './pages/ItemsPage';
 import ItemDetailPage from './pages/ItemDetailPage';
 import NotFoundPage from './pages/NotFoundPage';
+import CreateItemPage from './pages/CreateItemPage';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/items" element={<ItemsPage />} />
           <Route path="/items/:id" element={<ItemDetailPage />} />
+          <Route path="/items/new" element={<CreateItemPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Layout>
---
+++ client/src/pages/CreateItemPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CreateItemPage() {
+  return (
+    <div>
+      <h2>Add New Item</h2>
+      <p>Form tba...</p>
+    </div>
+  );
+}
+
+export default CreateItemPage;
