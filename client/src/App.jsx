c185dc6a1d53b15ffc3b2c6b37a4b74c33ee916f
Courtney Macgregor <cjoss90@hotmail.com>
2025-11-24 13:48:04 +1000

    feat(frontend): wire up top-level routing in App.jsx using BrowserRouter and move Layout into components folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import LoginPage from './pages/LoginPage';
+import ItemsPage from './pages/ItemsPage';
+import ItemDetailPage from './pages/ItemDetailPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
-    <div>
-      <h1>Medialog</h1>
-      <p>tba</p>
-    </div>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<ItemsPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/items" element={<ItemsPage />} />
+          <Route path="/items/:id" element={<ItemDetailPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
