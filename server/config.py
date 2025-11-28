ef6699c888664d5364f49a307588be1d10bb7e46
Courtney Macgregor <cjoss90@hotmail.com>
2025-11-28 14:23:50 +1000

    chore: support SMTP_* and MAIL_* env vars for email

--- server/config.py
+++ server/config.py
@@ -11,8 +11,29 @@ class Config:
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    MAIL_SERVER = os.environ.get("MAIL_SERVER", "sandbox.smtp.mailtrap.io")
-    MAIL_PORT = int(os.environ.get("MAIL_PORT", 587))
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    MAIL_FROM = os.environ.get("MAIL_FROM", "MediaLog <no-reply@medialog.test>")
+    MAIL_SERVER = (
+        os.environ.get("MAIL_SERVER")
+        or os.environ.get("SMTP_HOST")
+        or "sandbox.smtp.mailtrap.io"
+    )
+
+    MAIL_PORT = int(
+        os.environ.get("MAIL_PORT")
+        or os.environ.get("SMTP_PORT", 587)
+    )
+
+    MAIL_USERNAME = (
+        os.environ.get("MAIL_USERNAME")
+        or os.environ.get("SMTP_USERNAME")
+    )
+
+    MAIL_PASSWORD = (
+        os.environ.get("MAIL_PASSWORD")
+        or os.environ.get("SMTP_PASSWORD")
+    )
+
+    MAIL_FROM = (
+        os.environ.get("MAIL_FROM")
+        or os.environ.get("SMTP_FROM")
+        or "MediaLog <no-reply@medialog.test>"
+    )
